8011fbe3fc0c2868cabbac3d09bb7785d1e64d4b
webaprender <webaprendertv@gmail.com>
2021-10-15 01:17:49 -0500

    hola mundo

---
+++ hola-mundo.php
@@ -0,0 +1,8 @@
+<?php
+// https://github.com/webaprender/curso-php
+// escribir en la barra de direcciones del navegador web:
+// localhost:80/curso-php/hola-mundo.php 
+  
+  echo "Hola Mundo PHP";
+
+?>
