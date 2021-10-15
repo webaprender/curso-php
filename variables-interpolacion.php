123ba7f7316d6e24b68b8eff49634b9f5586afb8
webaprender <webaprendertv@gmail.com>
2021-10-15 02:19:17 -0500

    uso de interpolacion

---
+++ variables-interpolacion.php
@@ -0,0 +1,18 @@
+<?php
+  //declarar una variable
+  //inician por simbolo $
+  
+  //tipo de dato String
+  $nombre = "Ernesto";
+  
+  //tipo de dato String
+  $edad = 36;
+
+  //tipo de dato Float
+  $estatura = 1.71;
+
+  //interpolación de variables
+  //incrustar el valor de una variable 
+  //dentro de una cadena
+  echo "Mi nombre es $nombre tengo $edad años y mido $estatura m";
+?>
