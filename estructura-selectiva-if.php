c56616ca38655f91e3a824b703a293e22374403b
webaprender <webaprendertv@gmail.com>
2021-10-15 02:21:03 -0500

    comando if

---
+++ estructura-selectiva-if.php
@@ -0,0 +1,13 @@
+<?php
+  //tipo de dato boleano: 
+  $cerradura = "Si";
+  echo "cerradura: ".$cerradura."<br>";
+  //si la cerradura es igual a 1
+  if ($cerradura == "Si"){
+    //este mensaje se imprime si se cumple la condicion
+    echo "casa cerrada";
+  }else{
+     //este mensaje se imprime si NO se cumple la condicion
+     echo "casa abierta";
+  }
+?>
